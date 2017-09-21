727e482c826a19c951ca66d9c1b1f3fa07cafd21
10041004-1 <starsj128@gmail.com>
2017-09-21 14:48:56 +0900

    activity_login activity_match 수정

--- app/src/main/java/edu/android/teamproject/LoginActivity.java
+++ app/src/main/java/edu/android/teamproject/LoginActivity.java
@@ -4,27 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // 멤버변수
     ImageButton btn;
+    EditText et;
     Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btn = (ImageButton) findViewById(R.id.confirm);
+        btn = (ImageButton) findViewById(R.id.btn_next);
         intent = new Intent(this,MatchActivity.class);
+        et = (EditText) findViewById(R.id.edit_ID);
+        et = (EditText) findViewById(R.id.edit_PW);
 
         btn.setOnClickListener(this);
-    }
 
+    } // end onCreate()
+
+    // 로그인 버튼
     @Override
     public void onClick(View view) {
         startActivity(intent);
         finish();
-    }
-}
+    } // end onClick()
+} // end class LoginActivity
--- app/src/main/java/edu/android/teamproject/MatchActivity.java
+++ app/src/main/java/edu/android/teamproject/MatchActivity.java
@@ -1,16 +1,31 @@
 package edu.android.teamproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MatchActivity extends AppCompatActivity {
+
+    //멤버변수
+    ImageButton btn;
+    EditText et;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
 
+        btn = (ImageButton) findViewById(R.id.btn_calender);
+        btn = (ImageButton) findViewById(R.id.btn_link);
+        et = (EditText) findViewById(R.id.edit_my_phone);
+        et = (EditText) findViewById(R.id.edit_your_phone);
+        tv = (TextView) findViewById(R.id.text_start_day);
 
 
-    }
-}
+    } // end onCreate()
+
+} // end class MatchActivity
