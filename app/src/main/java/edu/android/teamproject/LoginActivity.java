f44a90f1f2a0c9a2cd909a00b28acdda96b0ad22
Jeong-Namju <zekallyang@naver.com>
2017-09-21 18:05:28 +0900

    UI 완성

--- app/src/main/java/edu/android/teamproject/LoginActivity.java
+++ app/src/main/java/edu/android/teamproject/LoginActivity.java
@@ -3,16 +3,19 @@ package edu.android.teamproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "edu.android";
+    private static final String KEY_MEMBER = "key_member";
     // 멤버변수
     ImageButton btn;
     EditText edit_ID,edit_PW;
-    Intent intent;
+
 
 
     @Override
@@ -21,7 +24,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         btn = (ImageButton) findViewById(R.id.imagebtn_login_next);
-        intent = new Intent(this,MatchActivity.class);
+
         edit_ID = (EditText) findViewById(R.id.edit_ID);
         edit_PW = (EditText) findViewById(R.id.edit_PW);
 
@@ -32,6 +35,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     // 로그인 버튼
     @Override
     public void onClick(View view) {
+        Intent intent = new Intent(this,MatchActivity.class);
+        String[] member = {
+                edit_ID.getText().toString(),
+                edit_PW.getText().toString()
+        };
+        intent.putExtra(KEY_MEMBER,member);
         startActivity(intent);
         finish();
     } // end onClick()
--- app/src/main/java/edu/android/teamproject/MatchActivity.java
+++ app/src/main/java/edu/android/teamproject/MatchActivity.java
@@ -3,6 +3,7 @@ package edu.android.teamproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -12,12 +13,13 @@ import android.widget.Toast;
 public class MatchActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String KEY_MEMBER = "key_member";
+    private static final String TAG = "edu.android";
     //멤버변수
-    private ImageButton btn_calender,
-                btn_link;
-    private EditText edit_my_phone,
-             edit_your_phone;
-    private TextView text_start_day;
+    private ImageButton imagebtn_match_calender,
+            imagebtn_match_link;
+    private EditText edit_match_my_phone,
+            edit_match_your_phone;
+    private TextView text_match_start_day;
 
     // Controller 객체 DiaryLab 생성
     DiaryLab dao = DiaryLab.getInstance();
@@ -28,14 +30,14 @@ public class MatchActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
 
-        btn_calender = (ImageButton) findViewById(R.id.imagebtn_match_calender);
-        btn_link = (ImageButton) findViewById(R.id.imagebtn_match_link);
-        edit_my_phone = (EditText) findViewById(R.id.edit_match_my_phone);
-        edit_your_phone = (EditText) findViewById(R.id.edit_match_your_phone);
-        text_start_day = (TextView) findViewById(R.id.text_match_start_day);
+        imagebtn_match_calender = (ImageButton) findViewById(R.id.imagebtn_match_calender);
+        imagebtn_match_link = (ImageButton) findViewById(R.id.imagebtn_match_link);
+        edit_match_my_phone = (EditText) findViewById(R.id.edit_match_my_phone);
+        edit_match_your_phone = (EditText) findViewById(R.id.edit_match_your_phone);
+        text_match_start_day = (TextView) findViewById(R.id.text_match_start_day);
 
-        btn_calender.setOnClickListener(this);
-        btn_link.setOnClickListener(this);
+        imagebtn_match_calender.setOnClickListener(this);
+        imagebtn_match_link.setOnClickListener(this);
 
     } // end onCreate()
 
@@ -46,20 +48,21 @@ public class MatchActivity extends AppCompatActivity implements View.OnClickList
 
         ImageButton btn = (ImageButton)view;
         // 다음 버튼을 클릭 했을 때
-        if(btn == btn_link){
+        if(btn == imagebtn_match_link){
 
             String id = member[0]; // id 값
             String pw = member[1]; // pw 값
-            String my = edit_my_phone.getText().toString(); // 내폰번
-            String your = edit_your_phone.getText().toString(); // 상대폰번
-            String startday = text_start_day.getText().toString(); // 사귄 날짜
+            String my = edit_match_my_phone.getText().toString(); // 내폰번
+            String your = edit_match_your_phone.getText().toString(); // 상대폰번
+            String startday = text_match_start_day.getText().toString(); // 사귄 날짜
             ModelMember m = new ModelMember(id,pw,my,your,startday); // 가져온 값 모델에 담아줌.
+            Log.i(TAG, m+"");
 
             dao.insertMember(m);
 
         }// 캘린더 버튼을 클릭 했을 때
-        else if(btn == btn_calender){
-
+        else if(btn == imagebtn_match_calender){
+            Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
         }// error
         else{
             Toast.makeText(this, "ERROR", Toast.LENGTH_SHORT).show();
