161235063bb7a15c33ff88e0b779a0a9bb010906
spuups <spuups@naver.com>
2017-09-26 14:37:59 +0900

    박종복

--- app/src/main/java/edu/android/teamproject/MainActivity.java
+++ app/src/main/java/edu/android/teamproject/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
-    private int i = 0;
+
     private static final String TAG = "edu.android";
     private static final String[] TAB_TITLES = {"일기쓰기", "일기장", "우리"};
 
