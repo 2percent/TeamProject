5da919c0fed2fefd00b79d468eb4c705730d906a
JohnFKennedy1 <vulnerable1324@gmail.com>
2017-09-28 15:11:30 +0900

    YWK

--- app/src/main/java/edu/android/teamproject/SettingActivity.java
+++ app/src/main/java/edu/android/teamproject/SettingActivity.java
@@ -2,18 +2,11 @@ package edu.android.teamproject;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.support.annotation.NonNull;
-import android.support.annotation.StringDef;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
@@ -21,7 +14,7 @@ public class SettingActivity extends AppCompatActivity {
 
     ExpandableRelativeLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4;
 
-    Button button2;
+    Button button1,button2,button3,button4;
 
     public ExpandableRelativeLayout getExpandableLayout2() {
         return expandableLayout2;
@@ -31,6 +24,15 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        button1 = (Button) findViewById(R.id.expandableButton1);
+        button3 = (Button) findViewById(R.id.expandableButton3);
+        button4 = (Button) findViewById(R.id.expandableButton4);
+
+        expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout1);
+        expandableLayout2 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout2);
+        expandableLayout3 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout3);
+        expandableLayout4 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout4);
 
         //      toolbar = (Toolbar)findViewById(R.id.toolBar);
         //      toolbar.setTitle("Expandable Layout Tutorial");
@@ -63,30 +65,75 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     public void expandableButton1(View view) {
-        expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout1);
         InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         keyboard.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        othersCollapse(view);
         expandableLayout1.toggle(); // toggle expand and collapse
     }
 
     public void expandableButton2(View view) {
-        expandableLayout2 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout2);
         InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         keyboard.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        othersCollapse(view);
         expandableLayout2.toggle(); // toggle expand and collapse
     }
 
     public void expandableButton3(View view) {
-        expandableLayout3 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout3);
         InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         keyboard.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        othersCollapse(view);
         expandableLayout3.toggle(); // toggle expand and collapse
     }
 
     public void expandableButton4(View view) {
-        expandableLayout4 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout4);
         InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         keyboard.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        othersCollapse(view);
         expandableLayout4.toggle(); // toggle expand and collapse
     }
+
+    private void othersCollapse(View view) {
+
+        if (view == button1){
+            if(expandableLayout2.isExpanded()){
+                expandableLayout2.collapse();
+            }
+            if(expandableLayout3.isExpanded()){
+                expandableLayout3.collapse();
+            }
+            if(expandableLayout4.isExpanded()){
+                expandableLayout4.collapse();
+            }
+        } else if (view == button2){
+            if(expandableLayout1.isExpanded()){
+                expandableLayout1.collapse();
+            }
+            if(expandableLayout3.isExpanded()){
+                expandableLayout3.collapse();
+            }
+            if(expandableLayout4.isExpanded()){
+                expandableLayout4.collapse();
+            }
+        } else if (view == button3){
+            if(expandableLayout1.isExpanded()){
+                expandableLayout1.collapse();
+            }
+            if(expandableLayout2.isExpanded()){
+                expandableLayout2.collapse();
+            }
+            if(expandableLayout4.isExpanded()){
+                expandableLayout4.collapse();
+            }
+        } else if (view == button4){
+            if(expandableLayout1.isExpanded()){
+                expandableLayout1.collapse();
+            }
+            if(expandableLayout2.isExpanded()){
+                expandableLayout2.collapse();
+            }
+            if(expandableLayout3.isExpanded()){
+                expandableLayout3.collapse();
+            }
+        }
+    } // end OthersCollopse
 }
